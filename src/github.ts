d317da1d72700e01ec076b5d5cbc38899b3b6407
Md Shahriyar Alam <contact@shahriyar.dev>
2023-12-07 00:52:09 +0600

    fix multi github stats

--- src/github.ts
+++ src/github.ts
@@ -2,30 +2,34 @@ import { RepoWithStats, getRepos, getStats } from "./github_helper"
 
 export const showGithubStats = async () => {
   const evalForm = document.querySelector(".assignment-evaluation-form")
-  console.log(evalForm)
   if (!evalForm) return
 
   const assignmentData = evalForm.querySelectorAll(
     ".row.form-group"
   )[1] as HTMLElement
-  console.log(assignmentData)
+
   if (!assignmentData) return
+  let github_container = document.getElementById("github_stats")
 
-  const github_container = document.createElement("div")
-  github_container.className = "col-md-12"
-  github_container.id = "github_stats"
-  github_container.style.marginBlock = "10px"
-  github_container.style.display = "flex"
-  github_container.style.flexDirection = "column"
-  github_container.style.gap = "10px"
-  github_container.innerText = "Loadig data..."
-
-  const content = assignmentData.innerText as string
-
+  if (!github_container) {
+    github_container = document.createElement("div")
+    github_container.className = "col-md-12"
+    github_container.id = "github_stats"
+    github_container.style.marginBlock = "10px"
+    github_container.style.display = "flex"
+    github_container.style.flexDirection = "column"
+    github_container.style.gap = "10px"
+    github_container.style.paddingInline = "15px"
+    github_container.innerText = "Loadig data..."
+  } else {
+    github_container.innerHTML = ""
+    github_container.innerText = "Loadig data..."
+  }
   assignmentData.appendChild(github_container)
 
+  const content = assignmentData.innerText as string
   const repos = getRepos(content)
-  console.log(repos)
+
   let githubToken = localStorage.getItem("github_token") as string
   if (!githubToken) {
     const token = prompt("Enter your github token here")
@@ -37,7 +41,6 @@ export const showGithubStats = async () => {
     }
   }
   const stats = await getStats(repos, githubToken)
-  console.log(stats)
   github_container.innerHTML = ""
 
   if (stats.error) {
@@ -45,13 +48,13 @@ export const showGithubStats = async () => {
   }
 
   const allData = stats.data as RepoWithStats[]
+
   for (const stat of allData) {
     const div = document.createElement("div")
     const repoName = document.createElement("h5")
     const lastCommit = document.createElement("p")
     const commitCount = document.createElement("p")
-    
-    div.style.paddingInline = "15px"
+
     lastCommit.style.margin = "0px"
     commitCount.style.margin = "0px"
 
