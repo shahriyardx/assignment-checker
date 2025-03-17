45feea053c147cfbac4bce4454004c373ee349c8
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2025-03-17 16:47:44 +0600

    updated version check

--- src/popup/pages/Update/NewVersionAvailable.tsx
+++ src/popup/pages/Update/NewVersionAvailable.tsx
@@ -4,23 +4,26 @@ import type { VersionInfo } from "./useVersion"
 import { RELEASE_URL } from "@/utils"
 
 const NewVersionAvailable = ({ versionInfo }: { versionInfo: VersionInfo }) => {
-  const { latestVersion } = versionInfo
+  const { latestVersion, changelog } = versionInfo
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col">
       <h3 className="text-2xl font-bold">⬇ Update Available</h3>
       <p>{latestVersion}</p>
-      <button
-        type="button"
-        onClick={() =>
-          chrome.tabs.create({
-            url: RELEASE_URL,
-          })
-        }
-        className="mt-3 px-3 py-2 bg-indigo-500 text-white rounded-md"
-      >
-        Download
-      </button>
+      <div className="prose prose-invert">{changelog}</div>
+      <div className="mt-3">
+        <button
+          type="button"
+          onClick={() =>
+            chrome.tabs.create({
+              url: RELEASE_URL,
+            })
+          }
+          className="px-3 py-2 bg-indigo-500 text-white rounded-md"
+        >
+          Download
+        </button>
+      </div>
     </div>
   )
 }
