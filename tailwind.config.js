860ec11124346e6d59dc36905f5c252931eb59a9
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2025-05-22 00:53:03 +0600

    fixed batch and assignment select to reflex new api changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: "media",
-  content: ["./src/**/*.{tsx,ts,html}"],
-  plugins: []
-}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "media",
+  content: ["./src/**/*.{tsx,ts,html}"],
+  plugins: []
+};
+
